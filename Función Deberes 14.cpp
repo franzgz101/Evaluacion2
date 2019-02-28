c66d99c095d9af58aef42c558ff94c6dbb8bdbe3
franzgz101 <43232417+franzgz101@users.noreply.github.com>
2019-02-28 16:24:13 +0100

    Add files via upload

---
+++ Función Deberes 14.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string.h> 
+using namespace std;
+
+int main(){
+    int alto;
+    int fil, col, k;
+    char salir;
+    cout<<"Como quieres que sea de alta la piramide: ";
+    cin>>alto;
+    
+    for(fil=0;fil<=alto;fil++){
+        for(col=0;col<=alto-fil;col++){
+            cout<<" ";}
+            for(k=0;k<=fil*2;k++){
+                cout<<"*";
+                  }
+                  
+        cout<<"\n";
+        
+    }
+    
+    
+   cout<<"\nToca cualquier tecla para salir:: ";
+    cin>>salir;
+    return 0;
+}
